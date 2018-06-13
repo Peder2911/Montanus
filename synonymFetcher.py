e10109151e6ba3d9090eb030d4b64e1d4b2373c4
Peder Landsverk <peder@vpn-client035.uio.no>
2018-06-13 16:51:16 +0200

    Added synonym fetcher

---
+++ synonymFetcher.py
@@ -0,0 +1,22 @@
+import requests
+import json
+import sys
+
+with open('config.json') as file:
+    config = json.loads(file.read())
+
+def getWikiPages(term):
+    baseUrl = config['wiki_base_url']+term+'&srlimit='+config['wiki_default_limit']
+    response = json.loads(requests.get(baseUrl).text)
+    return(response['query']['search'])
+
+def getWikiTitles(term):
+    result = getWikiPages(term)
+    titles = []
+    for page in result:
+        titles.append(page['title'])
+    return(titles)
+
+if __name__ == '__main__':
+    termRequest = sys.argv[1]
+    titles = getWikiTitles(termRequest)
