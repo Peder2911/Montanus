18181385d78db5fcfbb89c615c620d324f9969b6
Peder Landsverk <peder@Peders-MacBook-Pro.local>
2018-06-22 16:09:49 +0200

    Minor fixes

--- scraper.py
+++ scraper.py
@@ -21,14 +21,6 @@ from tools import pageTools,argvTools,urlTools,fileTools,dateTimeTools
 with open('config.json') as file:
     config = json.loads(file.read())
 
-# Returns a dictionary of URL components, also containing the API key
-# TODO Should maybe be somewhere else?
-
-def gatherComponents(target,config,keys):
-    components = config[target]
-    components['key'] = keys[target]
-    return(components)
-
 #####################################
 
 def getPage(url):
@@ -131,7 +123,7 @@ def subQuery(arguments,components,beginDate,endDate):
     elif pages != 0:
         articles = []
 
-        pagesToGet = [x+1 for x in range(pages)]
+        pagesToGet = [0]+[x+1 for x in range(pages)]
 
         for page in pagesToGet:
             url = urlTools.parseUrl(arguments,components,page,beginDate,endDate)
@@ -143,91 +135,9 @@ def subQuery(arguments,components,beginDate,endDate):
                 pass
                 #TODO error.
     else:
-        print('C')
         #TODO warning.
         articles = []
 
     return(articles)
 
 #####################################
-
-# Only actually needs targetSite and config, since these can give components
-# Only actually really needs target and args? (could read config in function)
-def scrapePages(targetSite,components,args,config,breaks = 0):
-
-    contentIndexer = pageTools.makeIndexer(targetSite,'contentPath',config)
-    hitsIndexer = pageTools.makeIndexer(targetSite,'hitsPath',config)
-    responseChecker = pageTools.makeChecker(targetSite,config)
-
-    firstUrl = urlTools.parseUrl(targetSite,components,args)
-    print('\nProbing: %s'%(firstUrl))
-    firstPage = requests.get(firstUrl)
-    firstPage = json.loads(firstPage.text)
-
-    if responseChecker(firstPage):
-        hits = hitsIndexer(firstPage)
-        pagesToGet = (hits-1) // 10
-    else:
-        # TODO better warning? (handled in pageTools?)
-        logging.warning('Exit 1 (see log)')
-        sys.exit(1)
-
-    if pagesToGet > 200:
-        breaks = (hits // 2000) + 1
-        scrapePages(targetSite,components,args,config,breaks = breaks)
-
-
-        # TODO time-range pageination
-        logging.warning('Too many pages to get, getting 200 pages')
-        pagesToGet = 200
-
-    #####################################
-
-    if hits > 0:
-        print('Number of hits: %s'%(hits))
-
-        print('Getting %i more pages' %(pagesToGet))
-        print('')
-
-        articles = contentIndexer(firstPage)
-
-        for pageNumber in range(pagesToGet):
-            page = getPage(urlTools.parseUrl(targetSite,components,args,pageNumber+1))
-
-            if responseChecker(page):
-                articles += contentIndexer(page)
-
-        return(articles)
-
-    else:
-        logging.warning('%s yielded no hits'%(str(args)))
-        return([])
-
-#####################################
-
-# This is the external function, that performs scraping.
-# Arguments are always given in list format!
-
-def directedScrape(targetSite,arguments,config,apiKeys):
-
-    # _
-    # Additional handling of arguments..
-    # _
-
-    # Adapting the query
-    queryArguments = argvTools.adaptQuery(targetSite,arguments,config)
-    urlComponents = gatherComponents(targetSite,config,apiKeys)
-
-    response=scrapePages(targetSite,urlComponents,queryArguments,config)
-
-    return(response)
-
-#####################################
-
-if __name__ == '__main__':
-
-    with open('config.json') as file:
-        config = json.loads(file.read())
-
-    with open('keys.json') as file:
-        apiKeys = json.loads(file.read())
