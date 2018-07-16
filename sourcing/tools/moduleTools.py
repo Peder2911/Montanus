bcc57010b870a685f68c959acf27906a7842128e
Peder Landsverk <pglandsverk@gmail.com>
2018-07-16 13:11:36 +0200

    structure overhaul

--- sourcing/tools/moduleTools.py
+++
@@ -1,9 +0,0 @@
-
-import os
-
-#####################################
-
-def relPath(filePath,fileVar):
-    selfPath = os.path.dirname(fileVar)
-    relPath = os.path.join(selfPath,filePath)
-    return(relPath)
